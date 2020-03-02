84a29433408214b098ebc729c1f46165d266f7d0
Matthew Jones <jonesmatt1991@gmail.com>
2020-03-02 17:22:52 +0800

    Initial Production Commit
    Signed-off-by: Matthew Jones <jonesmatt1991@gmail.com>

--- ShoppingList/ShoppingList/ShoppingList/Helpers/Constants.cs
+++ ShoppingList/ShoppingList/ShoppingList/Helpers/Constants.cs
@@ -6,7 +6,7 @@ namespace ShoppingList.Helpers
 {
     public class Constants
     {
-        public const string GET_ALL_ITEMS = "http://shopping-list-server.dev-apps.notsoslow.tk/items";
-        public const string ITEM = "http://shopping-list-server.dev-apps.notsoslow.tk/item/";
+        public const string GET_ALL_ITEMS = "http://shopping-list-server.prod-apps.notsoslow.tk/items";
+        public const string ITEM = "http://shopping-list-server.prod-apps.notsoslow.tk/item/";
     }
 }
